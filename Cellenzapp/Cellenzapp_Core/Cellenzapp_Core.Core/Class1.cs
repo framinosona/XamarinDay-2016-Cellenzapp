11336e43e6381b0d6128dcba3df6610965e89724
François Raminosona <framinosona@hotmail.Fr>
2016-09-09 14:14:59 +0200

    Arcitecture basique

---
+++ Cellenzapp/Cellenzapp_Core/Cellenzapp_Core.Core/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cellenzapp_Core.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Cellenzapp/Cellenzapp_Native/Cellenzapp_Native/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cellenzapp_Native
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
+
